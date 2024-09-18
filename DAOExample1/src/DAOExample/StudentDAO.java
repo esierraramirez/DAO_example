d55706aa3f2e9c93fa9ac2e47642d134548b35ff
esierraramirez <essierra62@ucatolica.edu.co>
2024-09-18 05:51:38 -0500

    DataAccessObject

--- DAOExample1/src/DAOExample/StudentDAO.java
+++ DAOExample1/src/DAOExample/StudentDAO.java
@@ -1,6 +1,12 @@
 
 package DAOExample;
 
-public class StudentDAO {
-    
+import java.util.List;
+
+public interface StudentDAO {
+    void addStudent(Student student);
+    Student getStudent(int id);
+    List<Student> getAllStudents();
+    void updateStudent(Student student);
+    void deleteStudent(int id);
 }
